092d68332ffa7098cbdd76b08b6c25b44360fc15
Enrico Marconi <enrico.marconi@hotmail.it>
2022-06-05 18:24:45 +0200

    WiP: Signup validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -7,41 +8,43 @@ const router = express.Router();
 // LOGIN ===============================
 // =====================================
 // show the login form
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
     // render the page and pass in any flash data if it exists
     res.render('login.ejs', { message: req.flash('loginMessage') });
 });
 
 // process the login form
 router.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/dashboard', // redirect to the secure dashboard section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+    successRedirect: '/dashboard', // redirect to the secure dashboard section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
 }));
 
 // =====================================
 // SIGNUP ==============================
 // =====================================
 // show the signup form
-router.get('/signup', function(req, res) {
+router.get('/signup', function (req, res) {
     // render the page and pass in any flash data if it exists
     res.render('signup.ejs', { message: req.flash('signupMessage') });
 });
 
 // process the signup form
 router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/dashboard', // redirect to the secure dashboard section
-    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}), function(req, res) {
-    console.log("hello");
-    console.log(req.body);
+    successRedirect: '/dashboard', // redirect to the secure dashboard section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}), body('email').isEmail(), body('password').isStrongPassword(), (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 });
 
 // =====================================
 // LOGOUT ==============================
 // =====================================
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
