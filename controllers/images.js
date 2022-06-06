9421d0d0672c2a4a809cd71869b185f99bbdd110
albertoz <zabeoalberto99@gmail.com>
2022-06-06 21:15:15 +0200

    fix: image regex

--- controllers/images.js
+++ controllers/images.js
@@ -1,36 +1,30 @@
+const regex = /^(?<repository>[\w.\-_]+((?::\d+|)(?=\/[a-z0-9._-]+\/[a-z0-9._-]+))|)(?:\/|)(?<image>[a-z0-9.\-_]+(?:\/[a-z0-9.\-_]+|))(:(?<tag>[\w.\-_]{1,127})|)$/;
+
 exports.getImage = function(req, res, next) {
-    let image = req.params.image.split('/');
+    let match = regex.exec(req.params.image);
     
-    if (image.length == 1) {
-        image_user = 'library';
-        image_name = image[0];
+    if (!match) {
+        res.status(400).send({'error': 'Invalid image'});
     } else {
-        image_user = image[0];
-        image_name = image[1];
-    }
-
-    image_name = image_name.split(':');
-
-    let image_tag = image_name[1] || 'latest'
-    image_name = image_name[0]
-
-
-    console.log(image_user, image_name, image_tag);
-    
-    fetch(`https://hub.docker.com/v2/repositories/${image_user}/${image_name}/tags/${image_tag}`, { method: "GET", headers: { "Content-Type": "application/json" } })
-        .then(reg_res => {
-            switch (reg_res.status) {
-                case 200:
-                    res.status(200).send(reg_res);
-                    break;
-                case 404:
-                    res.status(404).send({'error': 'Image not found'});
-                    break;
-                default:
-                    res.status(500).send({'error': 'Error fetching image'});
-            }
-        }).catch(err => {
-            console.log(err);
-            res.status(500).send({'error': 'Error fetching image tags'});
-        });
+        let repo = match.groups.repository || "";
+        let image = match.groups.image;
+        let tag = match.groups.tag || "latest";
+        
+        fetch(`https://hub.docker.com/v2/repositories/${image}/tags/${tag}`, { method: "GET", headers: { "Content-Type": "application/json" } })
+            .then(reg_res => {
+                switch (reg_res.status) {
+                    case 200:
+                        res.status(200).send(reg_res);
+                        break;
+                    case 404:
+                        res.status(404).send({'error': 'Image not found'});
+                        break;
+                    default:
+                        res.status(500).send({'error': 'Error fetching image'});
+                }
+            }).catch(err => {
+                console.log(err);
+                res.status(500).send({'error': 'Error fetching image tags'});
+            });
+        }
 }
