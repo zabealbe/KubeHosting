897a464cc7dd23100e57b770c95d9075d6b28c25
Enrico Marconi <31142849+UMR1352@users.noreply.github.com>
2022-06-06 23:27:14 +0200

    Feat input validation (#18)
    signup input validation and message report

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -7,41 +8,48 @@ const router = express.Router();
 // LOGIN ===============================
 // =====================================
 // show the login form
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
     // render the page and pass in any flash data if it exists
     res.render('login.ejs', { message: req.flash('loginMessage') });
 });
 
 // process the login form
 router.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/dashboard', // redirect to the secure dashboard section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+    successRedirect: '/dashboard', // redirect to the secure dashboard section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
 }));
 
 // =====================================
 // SIGNUP ==============================
 // =====================================
 // show the signup form
-router.get('/signup', function(req, res) {
+router.get('/signup', function (req, res) {
     // render the page and pass in any flash data if it exists
     res.render('signup.ejs', { message: req.flash('signupMessage') });
 });
 
+const PSWD_ERROR_MSG = "Password must contain at least 8 characters with at least: \n• 1 lowercase character \n• 1 uppercase character \n• 1 digit \n• 1 simbol";
+
 // process the signup form
-router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/dashboard', // redirect to the secure dashboard section
-    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}), function(req, res) {
-    console.log("hello");
-    console.log(req.body);
-});
+router.post('/signup', body('email').isEmail().withMessage('Invalid email address!'), body('password').isStrongPassword().withMessage(PSWD_ERROR_MSG), (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        errors.array().map(e => e.msg).forEach(msg => req.flash('signupMessage', msg));
+        return res.redirect("/signup");
+    } else {
+        next();
+    }
+}, passport.authenticate('local-signup', {
+    successRedirect: '/dashboard', // redirect to the secure dashboard section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}));
 
 // =====================================
 // LOGOUT ==============================
 // =====================================
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
